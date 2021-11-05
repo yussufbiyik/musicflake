fdc1cb4e082348758d88b6b450a58fbedc653b08
Yusuf Bıyık <yusufbyk3660@gmail.com>
2021-11-05 18:55:26 +0300

    Update button names + modal + button style

--- src/app/datasources/modal.datasource.ts
+++ src/app/datasources/modal.datasource.ts
@@ -3,7 +3,7 @@ import { Modal } from "src/app/classes/modal";
 export const Modals:Array<Modal> = [
     {
         id:'faq',
-        header:"🤔 How does this app work?",
+        header:"How does this app work? 🤔",
         content:"First of all the app asks for your location to learn the weather conditions on your area using open-meteo.com API.<br><br> Then the app will match your weathercode with a playlist group from our database and send you the link of a random playlist from that group.",
     }
 ]
