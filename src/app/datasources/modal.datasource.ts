bf51602ef03c05ce25265349f6f1405c2fdcf182
Yusuf Bıyık <yusufbyk3660@gmail.com>
2021-11-18 22:25:07 +0300

    ➕ Update functions & add error modal
    - Updated functions to make them more readable
    - Added an error modal to give better feedbacks about error

--- src/app/datasources/modal.datasource.ts
+++ src/app/datasources/modal.datasource.ts
@@ -5,5 +5,10 @@ export const Modals:Array<Modal> = [
         shorthand:'faq',
         header:"How does this app work? 🤔",
         content:"First of all the app asks for your location to learn the weather conditions on your area using open-meteo.com API.<br><br> Then the app will match your weathercode with a playlist group from our database and send you the link of a random playlist from that group.",
-    }
+    },
+    {
+        shorthand:'error',
+        header:"Something happened.",
+        content:"A problem just occured, can you share us how to reproduce this problem, you can do this by repeating what you just did and sharing it with us.",
+    },
 ]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,29 +17,18 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
   
-  openModal(modalShorthand:string, modal?:Modal){
+  openModal(modalShorthand:string){
     var documentModal = document.getElementById('modal')
     var backdropElement = document.getElementById('backdrop')
-    if(modalShorthand){
-      var modal = Modals.find((modal:Modal)=>{
-        if(modal.shorthand == modalShorthand){
-          return true;
-        }else{
-          return false
-        }
-      });
-      
-      if(modal){
-        var modalHeader = document.getElementById('modal-header')
-        var modalContent = document.getElementById('modal-content')
-        if(modalHeader && modalContent){
-          modalHeader.innerHTML = modal.header
-          modalContent.innerHTML = modal.content
-        }
-        documentModal?.classList.toggle('hidden')
-        backdropElement?.classList.toggle('invisible')
-      }
-    }
+    var modal = Modals.find((modal:Modal)=> modal.shorthand === modalShorthand)
+    if(!modal) {this.openModal('error'); return;}
+    var modalHeader = document.getElementById('modal-header')
+    var modalContent = document.getElementById('modal-content')
+    if(!modalHeader || !modalContent || !documentModal || !backdropElement) {this.openModal('error'); return;}
+    modalHeader.innerHTML = modal.header
+    modalContent.innerHTML = modal.content
+    documentModal.classList.toggle('hidden')
+    backdropElement.classList.toggle('invisible')
   }
 
   closeModal(){
@@ -54,49 +43,38 @@ export class HomeComponent implements OnInit {
     var backdropElement = document.getElementById('backdrop')
     var modalHeader = document.getElementById('modal-header')
     var modalContent = document.getElementById('modal-content')
-    if(modal && backdropElement){
-      if(modalHeader && modalContent){
-        modalHeader.innerHTML = modal.header
-        modalContent.innerHTML = modal.content
-      }
-      documentModal?.classList.toggle('hidden')
-      backdropElement?.classList.toggle('invisible')
-    }
+    if(!modalHeader || !modalContent || !documentModal || !backdropElement) {this.openModal('error'); return;}
+    modalHeader.innerHTML = modal.header
+    modalContent.innerHTML = modal.content
+    documentModal?.classList.toggle('hidden')
+    backdropElement?.classList.toggle('invisible')
   }
 
   recommendPlaylistBasedOffWeathercode(weathercode: Number){
     // Search for playlists with a compatible weathercode with users
-    let playlistMatches = Playlists.filter((playlist:Playlist)=>{
-      if(playlist.weathercodes.includes(weathercode)){
-        return true
-      }else{
-        return false
-      }
-    })
-
+    let playlistMatches = Playlists.filter((playlist:Playlist)=> playlist.weathercodes.includes(weathercode))
     // Select a random playlist from playlistMatches
     let recommendation = playlistMatches[Math.floor(Math.random()*playlistMatches.length)]
     return recommendation
   }
 
   suggestPlaylist(mode: string){
-    if(navigator.geolocation || navigator){
-      navigator.geolocation.getCurrentPosition((position) => {
-        var openmateoApiUrl: string = `https://api.open-meteo.com/v1/forecast?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&hourly=temperature_2m,relativehumitidy_2m,weathercode`
-        this.http.get<any>(openmateoApiUrl).subscribe((resp:any) => {
-          var currentTime = `${new Date().toISOString().substr(0,13)}:00`;
-          var currentTimeWeatherIndex = resp.hourly.time.findIndex((time:string) => time === currentTime)
-          var currentTimeWeathercode = resp.hourly.weathercode[currentTimeWeatherIndex]
-          var modal: Modal = {
-            shorthand:'playlist',
-            header:'🎆 Here is your playlist.',
-            content:`<iframe src="${this.recommendPlaylistBasedOffWeathercode(currentTimeWeathercode).uri}" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`,
-          }
-          this.createModal(modal)
-        })
-      })
-    }else{
+    if(!navigator.geolocation || !navigator) {
       alert('Your browser does not support geolocation 😢')
     }
+    navigator.geolocation.getCurrentPosition((position) => {
+      var openmateoApiUrl: string = `https://api.open-meteo.com/v1/forecast?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&hourly=temperature_2m,relativehumitidy_2m,weathercode`
+      this.http.get<any>(openmateoApiUrl).subscribe((resp:any) => {
+        var currentTime = `${new Date().toISOString().substr(0,13)}:00`
+        var currentTimeWeatherIndex = resp.hourly.time.findIndex((time:string) => time === currentTime)
+        var currentTimeWeathercode = resp.hourly.weathercode[currentTimeWeatherIndex]
+        var modal: Modal = {
+          shorthand:'playlist',
+          header:'🎆 Here is your playlist.',
+          content:`<iframe src="${this.recommendPlaylistBasedOffWeathercode(currentTimeWeathercode).uri}" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`,
+        }
+        this.createModal(modal)
+      })
+    })
   }
 }
