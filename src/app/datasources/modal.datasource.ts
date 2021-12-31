ad96a081a107e1912f64dfe4639dce45a390e7c9
Yusuf Bıyık <yusufbyk3660@gmail.com>
2021-12-31 18:33:13 +0300

    👌 Use emojis since the displaying issue fixed

--- src/app/datasources/modal.datasource.ts
+++ src/app/datasources/modal.datasource.ts
@@ -3,17 +3,17 @@ import { Modal } from "src/app/classes/modal";
 export const Modals:Array<Modal> = [
     {
         shorthand:'faq',
-        header:"How does this app work?",
-        content:"First of all the app asks for your location to learn the weather conditions on your area using open-meteo.com API.<br><br> Then the app will match your weathercode with a playlist group from our database and send you the link of a random playlist from that group.",
+        header:"<span class='emoji'>🤔</span> How does this app work?",
+        content:"First of all the app asks for your location to learn the weather conditions on your area using open-meteo.com API.<br>Then the app will try to match your weathercode with a playlist group from our database and send you the link of a random playlist from that group.",
     },
     {
         shorthand:'error',
-        header:"Something happened!",
+        header:"<span class='emoji'>😱</span> Something happened!",
         content:`A problem just occured, can you share us how to reproduce this problem, you can do this by repeating what you just did and sharing it with us.<br><br>{{param0}}`,
     },
     {
         shorthand:'playlist',
-        header:'Here is your playlist!',
+        header:"<span class='emoji'>🎁</span> Here is your playlist!",
         content:`
         <iframe src="{{param0}}" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         <br>
